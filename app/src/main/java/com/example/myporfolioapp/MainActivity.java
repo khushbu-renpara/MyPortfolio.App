1e7d7f8ce8c68841e21e55cdd0b05f0b29f447c1
Khushbu Renpara <jensirenpara29@gmali.com>
2026-01-28 14:20:55 +0530

    commit

---
+++ app/src/main/java/com/example/myporfolioapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myporfolioapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myporfolioapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
